4be8bfbb7c0f3eca48e60f021f46272a6b05b351
omarhegazy <omarbicboi@gmail.com>
2015-02-20 03:58:07 -0500

    Fixed run.py problem

--- src/run.py
+++ src/run.py
@@ -3,10 +3,10 @@ import sys
 
 if len(sys.argv) > 1:
 	if sys.argv[1] == "restart":
-		os.system("python init_scrape.py 1")
-		os.system("python get_artist_corpus.py 1")
-		os.system("python get_random_articles.py 1")
-		os.system("python get_random_corpus.py 1 ")
-		os.system("python language_test.py 1")
+		os.system("python init_scrape.py")
+		os.system("python get_artist_corpus.py")
+		os.system("python get_random_articles.py")
+		os.system("python get_random_corpus.py new")
+		os.system("python language_test.py")
 
-os.system("python get_pop.py 1")
+os.system("python get_pop.py")
